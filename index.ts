c1f059f784cf8b4626b58acc798b654ec7d53642
NickWang <brotherstyx@gmail.com>
2022-12-23 11:57:33 +0800

    feat: 🎸 basic code

---
+++ index.ts
@@ -0,0 +1,39 @@
+import { ScanStatus, WechatyBuilder } from 'wechaty'
+import QrcodeTerminal from 'qrcode-terminal'
+
+const token = 'puppet_workpro_example_token'
+const bot = WechatyBuilder.build({
+  puppet: 'wechaty-puppet-service',
+  puppetOptions: {
+    token,
+    tls: {
+      disable: true
+      // currently we are not using TLS since most puppet-service versions does not support it. See: https://github.com/wechaty/puppet-service/issues/160
+    }
+  }
+})
+
+bot.on('scan', (qrcode, status, data) => {
+  console.log(`
+  ============================================================
+  qrcode : ${qrcode}, status: ${status}, data: ${data}
+  ============================================================
+  `)
+  if (status === ScanStatus.Waiting) {
+    QrcodeTerminal.generate(qrcode, {
+      small: true
+    })
+  }
+}).on('login', user => {
+  console.log(`
+  ============================================
+  user: ${JSON.stringify(user)}, friend: ${user.friend()}, ${user.coworker()}
+  ============================================
+  `)
+}).on('message', message => {
+  console.log(`new message received: ${JSON.stringify(message)}`)
+}).on('error', err => {
+  console.log(err)
+})
+
+bot.start()
